325d2075a579aad2b72ee2700d3cc6ea999161ae
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2024-05-23 13:41:09 +0700

    .

--- flow/001/getsap.js
+++ flow/001/getsap.js
@@ -118,7 +118,7 @@ router.post('/getsap/getincomming', async (req, res) => {
 
 router.post('/getsap/getincomming_2', async (req, res) => {
   //-------------------------------------
-  console.log("----getsap/getincomming----");
+  console.log("----getsap/getincomming_2----");
   console.log(req.body);
   let input = req.body;
   //-------------------------------------
@@ -143,32 +143,7 @@ router.post('/getsap/getincomming_2', async (req, res) => {
     }
 
 
-    try {
-      let resp2 = await axios.post('http://tp-portal.thaiparker.co.th/API_QcReport/ZBAPI_getZPPIN013_OUT', {
-        "IMP_PRCTR": input[`IMP_PRCTR`],       //25000,25700 GW-GAS 
-        "IMP_WERKS": input[`IMP_WERKS`],        //2200 GW
-        "LAST_DATE": input[`LAST_DATE`],
-        "LAST_TIME": input[`LAST_TIME`]
-      });
-      if (resp2.status == 200) {
-        let returnDATA2 = resp2.data;
-        let  Good = returnDATA2["Rows"]||[]
-        for (let i = 0; i < output.length; i++) {
-          for (let j = 0; j < Good.length; j++) {
-
-           if(Good[j][`MATNR`] === output[i][`MATNR`] ){
-            output[i]['NAME1'] = Good[j][`NAME1`];
-            output[i]['SORTL'] = Good[j][`SORTL`];
-            break;
-           }
-            
-          }
-          
-        }
-      }
-    } catch (err) {
-      // output = [];
-    }
+   
 
   }
 
